2ff9114c3a9aaadad7853f6a8e09104a18cda166
3792274 <3792274@qq.com>
2017-01-07 20:03:48 +0800

    尚学堂完毕

---
+++ src/com/sxt/test/freemarker/web/TestAction.java
@@ -0,0 +1,25 @@
+package com.sxt.test.freemarker.web;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TestAction extends ActionSupport {
+    private static final long serialVersionUID = 1L;
+    
+    
+    private String companyName="尚学堂";
+	
+	public String execute() throws Exception {
+		System.out.println("TestAction.execute()");
+		return SUCCESS;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	
+	
+}
